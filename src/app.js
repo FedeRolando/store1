aba40b6e641a9aaa09f6325b1b4f9e6e05e66fad
Fede <federico@pircasolutions.com>
2023-03-25 16:50:32 -0300

    change mysql to mysql2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import mysql from "mysql"
+import mysql from "mysql2"
 import myConnection from "express-myconnection";
 //import dotenv from "dotenv";
 import routes from "./routes/index.js"
